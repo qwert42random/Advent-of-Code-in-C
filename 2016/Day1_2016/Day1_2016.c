c6d775be98bfa882478fe1653245d93d40d19071
Christopher <Cskywalker1@gmail.com>
2023-07-19 23:19:18 +0100

    Add: file reading support for Day1_2016.c

--- 2016/Day1_2016/Day1_2016.c
+++ 2016/Day1_2016/Day1_2016.c
@@ -1,14 +1,40 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-enum directions {
+enum directions
+{
     NORTH = 0,
     EAST = 1,
     SOUTH = 2,
     WEST = 3
 };
 
-int main(int argc, char *argv[]) {
+enum turn
+{
+    LEFT,
+    RIGHT
+};
+
+int main(int argc, char *argv[])
+{
     int DirectionFaced = NORTH;
-    printf("Hello world");
+    int turn = LEFT;
+
+    FILE *fptr;
+    fptr = fopen(argv[1], "r");
+
+    if (fptr == NULL)
+    {
+        printf("Error");
+        return 1;
+    }
+
+    char c;
+
+    while ((c = fgetc(fptr)) != EOF) {
+        printf("%c", c);
+    }
+
+    fclose(fptr);
     return 0;
 }
