886024970b54accd46d2089678c7de2b4782aa02
Christopher <Cskywalker1@gmail.com>
2023-07-19 21:18:10 +0100

    Add: files for Day1_2016
    - Add: Day1_2016.c.
    - Add: Input example texts.
    - Add: Input text.

---
+++ 2016/Day1_2016/Day1_2016.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+enum directions {
+    NORTH = 0,
+    EAST = 1,
+    SOUTH = 2,
+    WEST = 3
+};
+
+int main(int argc, char *argv[]) {
+    int DirectionFaced = NORTH;
+    printf("Hello world");
+    return 0;
+}
